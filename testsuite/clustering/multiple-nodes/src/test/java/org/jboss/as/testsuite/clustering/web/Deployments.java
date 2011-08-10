cc8bce513bba6131ecdb0870ebb8ec87063500a4
Karel Piwko <kpiwko@redhat.com>
2011-08-10 14:03:27 +0200

    AS7-1418 Test using multiple containers to verify clustering
    * Clustering TS switched to single and multiple node variants

---
+++ testsuite/clustering/multiple-nodes/src/test/java/org/jboss/as/testsuite/clustering/web/Deployments.java
@@ -0,0 +1,40 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.testsuite.clustering.web;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+/**
+ * Creates a deployment for distributable war
+ *
+ * @author <a href="kpiwko@redhat.com">Karel Piwko</a>
+ *
+ */
+public class Deployments {
+
+    public static WebArchive distributabeWar(String suffix) {
+        return ShrinkWrap.create(WebArchive.class, "distributable" + suffix + ".war")
+                .addClass(SimpleServlet.class)
+                .setWebXML("distributable.war/WEB-INF/web.xml");
+    }
+}
