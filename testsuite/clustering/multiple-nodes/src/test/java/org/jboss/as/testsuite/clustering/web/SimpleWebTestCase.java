121a296c2534aab3d253ccb396b2af740e4bd405
Karel Piwko <kpiwko@redhat.com>
2011-08-10 17:59:09 +0200

    AS7-1418 Added SessionId propagation

--- testsuite/clustering/multiple-nodes/src/test/java/org/jboss/as/testsuite/clustering/web/SimpleWebTestCase.java
+++ testsuite/clustering/multiple-nodes/src/test/java/org/jboss/as/testsuite/clustering/web/SimpleWebTestCase.java
@@ -28,6 +28,9 @@ import java.net.URL;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
@@ -52,6 +55,9 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 @RunAsClient
 public class SimpleWebTestCase {
+
+    private static String sessionId;
+
     @Deployment(name = "dep.active-1", testable = false)
     @TargetsContainer("container.active-1")
     public static Archive<?> deploymentOn1() {
@@ -79,14 +85,10 @@ public class SimpleWebTestCase {
             Assert.assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
             Assert.assertEquals(1, Integer.parseInt(response.getFirstHeader("value").getValue()));
             Assert.assertFalse(Boolean.valueOf(response.getFirstHeader("serialized").getValue()));
+
+            sessionId = extractSessionId(response);
             response.getEntity().getContent().close();
 
-            response = client.execute(new HttpGet(contextPath + "/simple"));
-            Assert.assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
-            Assert.assertEquals(2, Integer.parseInt(response.getFirstHeader("value").getValue()));
-            // This won't be true unless we have somewhere to which to replicate
-            Assert.assertTrue(Boolean.valueOf(response.getFirstHeader("serialized").getValue()));
-            response.getEntity().getContent().close();
         } finally {
             client.getConnectionManager().shutdown();
         }
@@ -97,9 +99,11 @@ public class SimpleWebTestCase {
     public void test2(@ArquillianResource URL contextPath) throws ClientProtocolException, IOException, URISyntaxException {
         DefaultHttpClient client = new DefaultHttpClient();
         try {
-            HttpResponse response = client.execute(new HttpGet(contextPath + "/simple"));
+            HttpGet request = new HttpGet(contextPath + "/simple");
+            attachSessionId(request);
+            HttpResponse response = client.execute(request);
             Assert.assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
-            Assert.assertEquals(3, Integer.parseInt(response.getFirstHeader("value").getValue()));
+            Assert.assertEquals(2, Integer.parseInt(response.getFirstHeader("value").getValue()));
             // This won't be true unless we have somewhere to which to replicate
             Assert.assertTrue(Boolean.valueOf(response.getFirstHeader("serialized").getValue()));
             response.getEntity().getContent().close();
@@ -113,12 +117,30 @@ public class SimpleWebTestCase {
     public void test3(@ArquillianResource URL contextPath) throws ClientProtocolException, IOException, URISyntaxException {
         DefaultHttpClient client = new DefaultHttpClient();
         try {
-            HttpResponse response = client.execute(new HttpGet(contextPath + "/simple"));
+            HttpGet request = new HttpGet(contextPath + "/simple");
+            attachSessionId(request);
+            HttpResponse response = client.execute(request);
             Assert.assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
-            Assert.assertEquals(4, Integer.parseInt(response.getFirstHeader("value").getValue()));
+            Assert.assertEquals(3, Integer.parseInt(response.getFirstHeader("value").getValue()));
             response.getEntity().getContent().close();
         } finally {
             client.getConnectionManager().shutdown();
         }
+    }
+
+    private void attachSessionId(HttpRequest request) {
+        request.addHeader("Cookie", "JSESSIONID=" + sessionId + "; Path=/distributable");
+    }
+
+    private String extractSessionId(HttpResponse response) {
+        for (Header header : response.getHeaders("Set-Cookie")) {
+            for (HeaderElement element : header.getElements()) {
+                String name = element.getName();
+                if ("JSESSIONID".equalsIgnoreCase(name)) {
+                    return element.getValue();
+                }
+            }
+        }
+        return null;
     }
 }
